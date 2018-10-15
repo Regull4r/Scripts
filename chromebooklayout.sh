4320996701c13ea549b8f2d0ca1b27ec87655ebb
Javier López <javilopeza9@gmail.com>
2018-10-15 20:09:15 +0200

    new chromebook layout

--- chromebooklayout.sh
+++ chromebooklayout.sh
@@ -3,17 +3,5 @@
 #Wait so xfce doesnt override keyboard settings
 sleep 3
 
-#Clear modifier keys
-xmodmap -e "remove Control = Control_L"
-xmodmap -e "remove Mod4 = Super_L"
-xmodmap -e "remove Mod1 = Alt_L"
-
-#Assign new keysyms
-xmodmap -e "keycode 64  = Super_L"
-xmodmap -e "keycode 133 = Control_L"
-xmodmap -e "keycode 37  = Alt_L"
-
-#Assign new functions
-xmodmap -e "add Mod4 = Super_L"
-xmodmap -e "add Control = Control_L"
-xmodmap -e "add Mod1 = Alt_L"
+setxkbmap -option ctrl:swap_rwin_rctl
+setxkbmap -option ctrl:swap_lwin_lctl
---
+++ oldchromebooklayout.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Wait so xfce doesnt override keyboard settings
+sleep 3
+
+#Clear modifier keys
+xmodmap -e "remove Control = Control_L"
+xmodmap -e "remove Mod4 = Super_L"
+xmodmap -e "remove Mod1 = Alt_L"
+
+#Assign new keysyms
+xmodmap -e "keycode 64  = Super_L"
+xmodmap -e "keycode 133 = Control_L"
+xmodmap -e "keycode 37  = Alt_L"
+
+#Assign new functions
+xmodmap -e "add Mod4 = Super_L"
+xmodmap -e "add Control = Control_L"
+xmodmap -e "add Mod1 = Alt_L"
